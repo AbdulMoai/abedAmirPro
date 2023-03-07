d181455759c1e266558d3e354f7633e179e3b382
AbdulMoati <abdsere5@gmail.com>
2023-03-07 15:02:49 -0800

    ..

--- end.js
+++ end.js
@@ -1,8 +1,8 @@
 const username = document.getElementById('username');
+const Email = document.getElementById('Email');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
@@ -12,6 +12,12 @@ finalScore.innerText = mostRecentScore;
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
+Email.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !Email.value;
+});
+
+
+
 
 saveHighScore = (e) => {
     e.preventDefault();
@@ -19,6 +25,7 @@ saveHighScore = (e) => {
     const score = {
         score: mostRecentScore,
         name: username.value,
+        Email: Email.value
     };
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
